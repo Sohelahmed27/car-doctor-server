a6d22583e75988bf21059d92aacb10cf06e7504c
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-05-17 11:41:49 +0600

    final-submit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
 
@@ -12,9 +13,9 @@ const port = process.env.port || 5000;
 
 
  console.log(process.env.DB_PASSWORD)
-const uri =`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.fbfgu.mongodb.net:27017,cluster0-shard-00-01.fbfgu.mongodb.net:27017,cluster0-shard-00-02.fbfgu.mongodb.net:27017/?ssl=true&replicaSet=atlas-m0zpta-shard-0&authSource=admin&retryWrites=true&w=majority`
+// const uri =`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.fbfgu.mongodb.net:27017,cluster0-shard-00-01.fbfgu.mongodb.net:27017,cluster0-shard-00-02.fbfgu.mongodb.net:27017/?ssl=true&replicaSet=atlas-m0zpta-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbfgu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbfgu.mongodb.net/?retryWrites=true&w=majority`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +27,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  console.log('Hitting verify')
+  console.log(req.headers.authorization)
+  const authorization = req.headers.authorization;
+
+  if(!authorization){
+    return res.status(401).send({error:true, message: 'Invalid authorization'})
+  }
+  const token = authorization.split(' ')[1]
+  console.log('Verifying token JWT', token)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err, decoded)=>{
+    if(err){
+      return res.status(401).send({err:true, message:'Invalid authorization'})
+    }
+    req.decoded = decoded;
+    next();
+
+
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -36,7 +58,17 @@ async function run() {
     const userCollection =  client.db('carDoctor').collection('services')
     const bookingCollection = client.db('carDoctors').collection('bookings')
 
+    //jwt 
+    app.post('/jwt', (req,res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'} )
+      res.send({token});
+    })
 
+
+
+   //SERVICE routes
     app.get('/services', async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
@@ -56,8 +88,16 @@ async function run() {
     })
 
     //find bookings from database
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log('came back after verify', decoded);
+
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({ error: 1, message:'forbidden  access'})
+      }
+      // console.log(req.headers.authorization)
       let query={};
+      
       if(req.query?.email){
         query = {email:req.query.email};
       }
